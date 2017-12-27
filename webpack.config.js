5b8b7030494ef83824779c6e1e30bcd30ebe11b7
李杨 <liyang@liyangdeMacBook-Pro.local>
2017-12-27 16:36:07 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+if(process.env.NODE_ENV == 'development'){
+    module.exports = require('./webpack.config.dev.js');
+}else if(process.env.NODE_ENV == 'depley'){
+    module.exports = require('./webpack.config.deploy.js');
+}else if(process.env.NODE_ENV == 'release'){
+    module.exports = require('./webpack.config.release.js');
+}
